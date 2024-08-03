506f0d9d7164c3692309942e5adbed673935a10c
xylonity <loldanielespia@gmail.com>
2024-08-03 19:23:01 +0200

    :bug: Fixed a bug where recipes wouldn't work

--- src/main/java/net/xylonity/knightquest/registry/KnightQuestSounds.java
+++ src/main/java/net/xylonity/knightquest/registry/KnightQuestSounds.java
@@ -4,15 +4,16 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import net.xylonity.knightquest.KnightQuest;
+
+import java.util.function.Supplier;
 
 public class KnightQuestSounds {
     public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, KnightQuest.MOD_ID);
 
-    public static final RegistryObject<SoundEvent> NETHERMAN_BOSS_MUSIC = register("netherman_boss_music");
+    public static final Supplier<SoundEvent> NETHERMAN_BOSS_MUSIC = register("netherman_boss_music");
 
-    private static RegistryObject<SoundEvent> register(String name) {
+    private static Supplier<SoundEvent> register(String name) {
         return SOUNDS.register(name, () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(KnightQuest.MOD_ID, name)));
     }
 }

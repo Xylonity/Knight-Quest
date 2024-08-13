6580840decad71a338d77a3e3a9bf6584e798e33
xylonity <loldanielespia@gmail.com>
2024-08-13 18:01:14 +0200

    :wrench: Deleted explosive config

--- src/main/java/net/xylonity/knightquest/common/api/explosiveenhancement/ExplosiveValues.java
+++ src/main/java/net/xylonity/knightquest/common/api/explosiveenhancement/ExplosiveValues.java
@@ -1,31 +1,23 @@
 package net.xylonity.knightquest.common.api.explosiveenhancement;
 
-import net.neoforged.fml.loading.FMLPaths;
-
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 public class ExplosiveValues {
-    static Path CONFIG_PATH = FMLPaths.CONFIGDIR.get().resolve("explosiveenhancement.toml");
-    private static final boolean V = Files.exists(CONFIG_PATH);
-    public static boolean showBlastWave = V ? ExplosiveEnhancementConfig.showBlastWave.get() : true;
-    public static boolean showFireball = V ? ExplosiveEnhancementConfig.showFireball.get() : true;
-    public static boolean showMushroomCloud = V ? ExplosiveEnhancementConfig.showMushroomCloud.get() : true;
-    public static boolean showSparks = V ? ExplosiveEnhancementConfig.showSparks.get() : true;
-    public static double sparkSize = V ? ExplosiveEnhancementConfig.sparkSize.get() : 5.3;
-    public static double sparkOpacity = V ? ExplosiveEnhancementConfig.sparkOpacity.get() : 0.70;
-    public static boolean showShockwave = V ? ExplosiveEnhancementConfig.showShockwave.get() : true;
-    public static boolean showUnderwaterBlastWave = V ? ExplosiveEnhancementConfig.showUnderwaterBlastWave.get() : true;
-    public static int bubbleAmount = V ? ExplosiveEnhancementConfig.bubbleAmount.get() : 50;
-    public static boolean showUnderwaterSparks = V ? ExplosiveEnhancementConfig.showUnderwaterSparks.get() : false;
-    public static double underwaterSparkSize = V ? ExplosiveEnhancementConfig.underwaterSparkSize.get() : 4.0;
-    public static double underwaterSparkOpacity = V ? ExplosiveEnhancementConfig.underwaterSparkOpacity.get() : 0.30;
-    public static boolean dynamicSize = V ? ExplosiveEnhancementConfig.dynamicSize.get() : true;
-    public static boolean dynamicUnderwater = V ? ExplosiveEnhancementConfig.dynamicUnderwater.get() : true;
-    public static boolean attemptBetterSmallExplosions = V ? ExplosiveEnhancementConfig.attemptBetterSmallExplosions.get() : true;
-    public static double smallExplosionYOffset = V ? ExplosiveEnhancementConfig.smallExplosionYOffset.get() : -0.5;
-    public static boolean emissiveExplosion = V ? ExplosiveEnhancementConfig.emissiveExplosion.get() : true;
-    public static boolean emissiveWaterExplosion = V ? ExplosiveEnhancementConfig.emissiveWaterExplosion.get() : true;
-    public static boolean alwaysShow = V ? ExplosiveEnhancementConfig.alwaysShow.get() : false;
-
+    public static boolean showBlastWave = true;
+    public static boolean showFireball = true;
+    public static boolean showMushroomCloud = true;
+    public static boolean showSparks = true;
+    public static double sparkSize = 5.3;
+    public static double sparkOpacity = 0.70;
+    public static boolean showShockwave = true;
+    public static boolean showUnderwaterBlastWave = true;
+    public static int bubbleAmount = 50;
+    public static boolean showUnderwaterSparks = false;
+    public static double underwaterSparkSize = 4.0;
+    public static double underwaterSparkOpacity = 0.30;
+    public static boolean dynamicSize = true;
+    public static boolean dynamicUnderwater = true;
+    public static boolean attemptBetterSmallExplosions = true;
+    public static double smallExplosionYOffset = -0.5;
+    public static boolean emissiveExplosion = true;
+    public static boolean emissiveWaterExplosion = true;
+    public static boolean alwaysShow = false;
 }

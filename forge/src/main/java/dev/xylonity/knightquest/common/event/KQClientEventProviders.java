b3e7f0ecf9b9c61745e027978a13e08239da2641
xylonity <loldanielespia@gmail.com>
2024-12-21 23:31:44 +0100

    💴 Deleted client-sided render registries of explosiveenhancement

--- forge/src/main/java/dev/xylonity/knightquest/common/event/KQClientEventProviders.java
+++ forge/src/main/java/dev/xylonity/knightquest/common/event/KQClientEventProviders.java
@@ -3,11 +3,6 @@ package dev.xylonity.knightquest.common.event;
 import dev.xylonity.knightquest.KnightQuest;
 import dev.xylonity.knightquest.client.entity.renderer.*;
 import dev.xylonity.knightquest.common.particle.*;
-import dev.xylonity.knightquest.common.particle.explosiveenhancement.*;
-import dev.xylonity.knightquest.common.particle.explosiveenhancement.blue.BlueBlastWaveParticle;
-import dev.xylonity.knightquest.common.particle.explosiveenhancement.blue.BlueFireballParticle;
-import dev.xylonity.knightquest.common.particle.explosiveenhancement.red.RedBlastWaveParticle;
-import dev.xylonity.knightquest.common.particle.explosiveenhancement.red.RedFireballParticle;
 import dev.xylonity.knightquest.registry.KnightQuestEntities;
 import dev.xylonity.knightquest.registry.KnightQuestParticles;
 import net.minecraft.client.renderer.entity.EntityRenderers;
@@ -28,27 +23,6 @@ public class KQClientEventProviders {
         event.registerSpriteSet(KnightQuestParticles.SNOWFLAKE_PARTICLE.get(), SnowflakeParticle.Provider::new);
         event.registerSpriteSet(KnightQuestParticles.POISON_PARTICLE.get(), PoisonParticle.Provider::new);
         event.registerSpriteSet(KnightQuestParticles.POISON_CLOUD_PARTICLE.get(), PoisonCloudParticle.Provider::new);
-
-        /*
-         * Explosive Enhancement particle registers
-         */
-
-        event.registerSpriteSet(KnightQuestParticles.BLASTWAVE.get(), BlastWaveParticle.Provider::new);
-        event.registerSpriteSet(KnightQuestParticles.FIREBALL.get(), FireballParticle.Provider::new);
-        event.registerSpriteSet(KnightQuestParticles.BLANK_FIREBALL.get(), FireballParticle.Provider::new);
-        event.registerSpriteSet(KnightQuestParticles.SMOKE.get(), SmokeParticle.Provider::new);
-        event.registerSpriteSet(KnightQuestParticles.SPARKS.get(), SparksParticle.Provider::new);
-        event.registerSpriteSet(KnightQuestParticles.BUBBLE.get(), BubbleParticle.Provider::new);
-        event.registerSpriteSet(KnightQuestParticles.SHOCKWAVE.get(), ShockwaveParticle.Provider::new);
-        event.registerSpriteSet(KnightQuestParticles.BLANK_SHOCKWAVE.get(), ShockwaveParticle.Provider::new);
-        event.registerSpriteSet(KnightQuestParticles.UNDERWATERBLASTWAVE.get(), UnderwaterBlastwaveParticle.Provider::new);
-        event.registerSpriteSet(KnightQuestParticles.UNDERWATERSPARKS.get(), UnderwaterSparksParticle.Provider::new);
-
-        event.registerSpriteSet(KnightQuestParticles.BLUEBLASTWAVE.get(), BlueBlastWaveParticle.Provider::new);
-        event.registerSpriteSet(KnightQuestParticles.BLUEFIREBALL.get(), BlueFireballParticle.Provider::new);
-
-        event.registerSpriteSet(KnightQuestParticles.REDBLASTWAVE.get(), RedBlastWaveParticle.Provider::new);
-        event.registerSpriteSet(KnightQuestParticles.REDFIREBALL.get(), RedFireballParticle.Provider::new);
     }
 
     @SubscribeEvent

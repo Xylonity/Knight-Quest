aa186a3fcee9eca0584d2f8d9036937eae046dcc
xylonity <42015327+Xylonity@users.noreply.github.com>
2026-06-15 18:10:25 +0200

    🐛 Fixed a case where hostile entities wouldn't drop green essence even though knight quest was in the pack

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+# 1.9.3
+- Fixed a case where hostile entities wouldn't drop green essence even though knight quest was in the pack
+
 # 1.9.2
 - Added compat with knightlib 1.5.0
 - Preparing the ground for a major update
--- common/src/main/java/dev/xylonity/knightquest/KnightQuestCommon.java
+++ common/src/main/java/dev/xylonity/knightquest/KnightQuestCommon.java
@@ -1,5 +1,6 @@
 package dev.xylonity.knightquest;
 
+import dev.xylonity.knightlib.KnightLib;
 import dev.xylonity.knightquest.common.material.KQArmorMaterials;
 import dev.xylonity.knightquest.platform.KnightQuestPlatform;
 import dev.xylonity.knightquest.registry.*;
@@ -22,6 +23,12 @@ public class KnightQuestCommon {
         KnightQuestParticles.init();
         KnightQuestCreativeModeTabs.init();
         KnightQuestEntities.init();
+
+        KnightLib.initialize(
+                KnightLib.Usage.COPPER_GRAILS,
+                KnightLib.Usage.GREAT_CHALICE,
+                KnightLib.Usage.GREEN_ESSENCES
+        );
     }
 
 }
